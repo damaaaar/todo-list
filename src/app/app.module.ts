8d3cb1620ca5f12a189fc4ea01c845c45c4a1e94
Daniel Martínez <danielmartinez.damar@hotmail.com>
2025-11-20 14:00:58 -0600

    [ADD] todo module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { TodoModule } from './todo/todo.module'
 
 import { AppComponent } from './app.component'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TodoModule],
   providers: [],
   bootstrap: [AppComponent],
 })
